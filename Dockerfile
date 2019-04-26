7906379cafc291d8dd5dd9c38f336fe203765baf
Takács Ákos <rimelek@rimelek.hu>
2019-04-26 22:11:53 +0200

    new build script

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
-ARG BUILDER_IMAGE="dspace-builder"
-ARG DSPACE_IMAGE="dspace-tomcat"
+ARG BUILDER_IMAGE="localhost/dspace/dspace-builder"
+ARG TOMCAT_IMAGE="localhost/dspace/dspace-tomcat"
 
 FROM ${BUILDER_IMAGE} as builder
 
-FROM ${DSPACE_IMAGE}
+FROM ${TOMCAT_IMAGE}
+
+ARG GIT_COMMIT=""
+
+LABEL hu.itsziget.dspace-tomcat-build.git-commit=$GIT_COMMIT
+
+RUN if [ -z "${GIT_COMMIT}" ]; then >&2 echo "Missing build argument: GIT_COMMIT"; exit 1; fi;
--- build.sh
+++ build.sh
@@ -1,14 +1,121 @@
 #!/usr/bin/env bash
 
-TAG=${1}
-APP_NAME=${2:-xmlui}
+TAG=""
+OWNER=""
+REGISTRY=""
+APP_NAME=""
+BUILD_NAME=""
+
+ltrim() {
+    local STR="$(echo "${@:-"$(cat -)"}")"
+    echo "${STR}" | sed -e 's/^[[:space:]]*//'
+}
+
+rtrim() {
+    local STR="$(echo "${@:-"$(cat -)"}")"
+    echo "${STR}" | sed -e 's/[[:space:]]*$//'
+}
+
+trim() {
+    local STR="$(echo "${@:-"$(cat -)"}")"
+    rtrim "$(ltrim "${STR}")"
+}
+
+usage() {
+    echo "Usage: $0 [-r <REGISTRY>] [-o <OWNER>] [-t <TAG>] [-a <APP_NAME>]"
+    echo
+    echo "  -a <APP_NAME>    The name of the DSpace app in case you want to build an application image too."
+    echo "                   You can pass multiple names but then you need to specify a build name using the option -b. "
+    echo "  -b <BUILD_NAME>  If APP_NAME contains only one app then it is the name of the app by default."
+    echo "  -o <OWNER>       The owner of the image."
+    echo "  -r <REGISTRY>    The name of the registry. (\"localhost\" by default)"
+    echo "  -t <TAG>         The tag of the image."
+    echo
+    echo "The patterns of the final images name:"
+    echo "builder: <REGISTRY>/<OWNER>/dspace-builder:<TAG>"
+    exit 1
+}
+
+while getopts ':t:a:b:o:r:h' opt; do
+    case "${opt}" in
+        a)
+            APP_NAME="${OPTARG}"
+            ;;
+        b)
+            BUILD_NAME="${OPTARG}"
+            ;;
+        t)
+            TAG="${OPTARG}";
+            ;;
+        o)
+            OWNER="${OPTARG}"
+            ;;
+        r)
+            REPOSITORY="${OPTARG}"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            >&2 echo "Invalid option: ${opt}"
+            >&2 usage
+            exit 1
+    esac
+done
+shift $((OPTIND-1))
 
 if [[ -z "${TAG}" ]]; then
-    >&2 echo "Missing first argument"
+    >&2 echo "Required option: -t <TAG>"
     exit 1
-fi;
+fi
 
+if [[ -z "${OWNER}" ]]; then
+    OWNER="dspace"
+fi
+
+if [[ -z "${REGISTRY}" ]]; then
+    REGISTRY="localhost"
+fi
+
+if [[ -z "${APP_NAME}" ]]; then
+    >&2 echo "Required option: -a <APP_NAME>"
+    exit 1
+fi
+
+APP_NAME="$(trim "${APP_NAME}")"
+
+if [[ -z "${BUILD_NAME}" ]]; then
+    if [[ ! "${APP_NAME}" =~ *" "* ]]; then
+        BUILD_NAME="${APP_NAME}"
+    else
+        >&2 echo "-b <BUILD_NAME> is required when APP_NAME contains multiple applications"
+        ecit 1
+    fi
+fi
+
+# build the builder
+BUILDER_IMAGE="${REGISTRY}/${OWNER}/dspace-builder:${TAG}"
 docker build \
-    -t itsziget/dspace-${APP_NAME}:${TAG} \
+    -t "${BUILDER_IMAGE}" \
+    -f builder.Dockerfile \
     --build-arg GIT_COMMIT=$(git rev-parse HEAD) \
-    --build-arg APP_NAME=${APP_NAME} .
+    .
+
+# build the base tomcat
+TOMCAT_IMAGE="${REGISTRY}/${OWNER}/dspace-tomcat:${TAG}"
+docker build \
+    -t "${TOMCAT_IMAGE}" \
+    -f tomcat.Dockerfile \
+    --build-arg GIT_COMMIT=$(git rev-parse HEAD) \
+    .
+
+if [[ -n "${APP_NAME}" ]]; then
+    docker build \
+        -t ${REGISTRY}/${OWNER}/dspace-tomcat-${BUILD_NAME} \
+        --build-arg GIT_COMMIT=$(git rev-parse HEAD) \
+        --build-arg APP_NAME="${APP_NAME}" \
+        --build-arg BUILDER_IMAGE="${BUILDER_IMAGE}" \
+        --build-arg DSPACE_IMAGE="${TOMCAT_IMAGE}" \
+        .
+fi
